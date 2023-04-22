bac475a84d8e9b399a59d6c9426d71d64bf0a13e
AlphaSoundZ <jan.jacob.holst@gmail.com>
2023-04-22 14:25:27 +0200

    ✨ feat: design fixes

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,17 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <RouterView />
-  <div class="gradient"></div>
+  <div class="background-container">
+    <div class="gradient"></div>
+    <div class="static-background"></div>
+  </div>
 </template>
 
 <style lang="scss">
-RouterLink {
+RouterView {
   color: #42b983;
   font-weight: bold;
   text-decoration: none;
-
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
 }
 
 .gradient {
@@ -37,7 +35,31 @@ RouterLink {
   animation: rotate var(--speed) var(--easing) alternate infinite;
   border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
 
-  z-index: -1;
+  z-index: -10;
+}
+
+.background-container {
+  position: absolute;
+  overflow: hidden;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+}
+
+.static-background {
+  background: rgb(63, 161, 251);
+  background-image: linear-gradient(
+    142deg,
+    rgba(63, 161, 251, 1) 0%,
+    rgba(252, 70, 168, 1) 100%
+  );
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -11;
 }
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,264 +1,49 @@
-<template>
-    <main>
-        <h1>Core-Workout</h1>
-        <h3 class="subtitle">von Emil, Maxim und Jan Jacob</h3>
-        
-        <RouterLink to="/1">
-            <button class='glowing-btn'><span class='glowing-txt'>S<span class='faulty-letter'>T</span>ART</span></button>
-        </RouterLink>
-    </main>
+<template class="view">
+  <div class="container">
+    <div class="title-container">
+      <h1>Core-Workout</h1>
+      <h3 class="subtitle">von Emil, Maxim und Jan Jacob</h3>
+    </div>
+    <RouterLink to="/1">
+        <button class='glowing-btn'><span class='glowing-txt'>S<span class='faulty-letter'>T</span>ART</span></button>
+    </RouterLink>
+  </div>
 </template>
+
 
 <style lang="scss">
 
 h1 {
-    margin-bottom: 0;
-    z-index: 1;
-    line-height: 1;
+  margin-bottom: 0;
+  z-index: 1;
+  line-height: 1;
 }
 
 h3 {
-    margin-top: 5vh;
-    margin-bottom: 40vh;
-    z-index: 1;
-}
-
-input {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    color: rgb(0, 0, 0);
-    font-size: 5rem;
-    z-index: 1;
-    font-family: 'Roboto', sans-serif;
-    white-space: nowrap;
-    border: 0;
-    border-radius: 50rem;
-    cursor: pointer;
-    outline: none;
-    padding: 3rem 5rem;
-
-    // gradient animated hover with sweeping edge
-    background: linear-gradient(90deg, #f6d365 0%, #fda085 50%, #f6d365 100%);
-    background-size: 200%;
-    transition: background 0.8s ease-in-out;
-
-    // sweeping edge glow effect
-    box-shadow: 0 0 10px #f6d365, 0 0 40px #f6d365, 0 0 80px #f6d365;
-    animation: glowing 1.5s ease-in-out infinite alternate;
-}
-
-input:hover {
-    // gradient animated hover with sweeping edge
-    background-position: 100%;
-    background-size: 200%;
-    transition: background 0.8s ease-in-out;
-
-    // sweeping edge glow effect
-    box-shadow: 0 0 10px #f6d365, 0 0 40px #f6d365, 0 0 80px #f6d365;
-    animation: glowing 1.5s ease-in-out infinite alternate;
+  margin-top: 5%;
+  z-index: 1;
 }
 
 :root {
   --glow-color: hsl(186 100% 69%);
 }
 
-.glowing-btn {
-  position: relative;
-  color: var(--glow-color);
-  cursor: pointer;
-  padding: 0.35em 1em;
-  border: 0.15em solid var(--glow-color);
-  border-radius: 0.45em;
-  background: none;
-  perspective: 2em;
-  font-family: "Raleway", sans-serif;
-  font-size: 3em;
-  font-weight: 900;
-  letter-spacing: 1em;
-
-  -webkit-box-shadow: inset 0px 0px 0.5em 0px var(--glow-color),
-    0px 0px 0.5em 0px var(--glow-color);
-  -moz-box-shadow: inset 0px 0px 0.5em 0px var(--glow-color),
-    0px 0px 0.5em 0px var(--glow-color);
-  box-shadow: inset 0px 0px 0.5em 0px var(--glow-color),
-    0px 0px 0.5em 0px var(--glow-color);
-  animation: border-flicker 2s linear infinite;
-}
-
-.glowing-txt {
-  float: left;
-  margin-right: -0.8em;
-  -webkit-text-shadow: 0 0 0.125em hsl(0 0% 100% / 0.3),
-    0 0 0.45em var(--glow-color);
-  -moz-text-shadow: 0 0 0.125em hsl(0 0% 100% / 0.3),
-    0 0 0.45em var(--glow-color);
-  text-shadow: 0 0 0.125em hsl(0 0% 100% / 0.3), 0 0 0.45em var(--glow-color);
-  animation: text-flicker 3s linear infinite;
-}
-
-.faulty-letter {
-  opacity: 0.5;
-  animation: faulty-flicker 2s linear infinite;
-}
-
-.glowing-btn::before {
-  content: "";
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  opacity: 0.7;
-  filter: blur(1em);
-  transform: translateY(120%) rotateX(95deg) scale(1, 0.35);
-  background: var(--glow-color);
-  pointer-events: none;
-}
-
-.glowing-btn::after {
-  content: "";
+.container {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  
+  // center div in body
   position: absolute;
   top: 0;
   left: 0;
-  right: 0;
   bottom: 0;
-  opacity: 0;
-  z-index: -1;
-  background-color: var(--glow-color);
-  box-shadow: 0 0 2em 0.2em var(--glow-color);
-  transition: opacity 100ms linear;
+  right: 0;
+  margin: auto;
 }
-
-.glowing-btn:hover {
-  color: rgba(0, 0, 0, 0.8);
-  text-shadow: none;
-  animation: none;
-}
-
-.glowing-btn:hover .glowing-txt {
-  animation: none;
-}
-
-.glowing-btn:hover .faulty-letter {
-  animation: none;
-  text-shadow: none;
-  opacity: 1;
-}
-
-.glowing-btn:hover:before {
-  filter: blur(1.5em);
-  opacity: 1;
-}
-
-.glowing-btn:hover:after {
-  opacity: 1;
-}
-
-@keyframes faulty-flicker {
-  0% {
-    opacity: 0.1;
-  }
-  2% {
-    opacity: 0.1;
-  }
-  4% {
-    opacity: 0.5;
-  }
-  19% {
-    opacity: 0.5;
-  }
-  21% {
-    opacity: 0.1;
-  }
-  23% {
-    opacity: 1;
-  }
-  80% {
-    opacity: 0.5;
-  }
-  83% {
-    opacity: 0.4;
-  }
-
-  87% {
-    opacity: 1;
-  }
-}
-
-@keyframes text-flicker {
-  0% {
-    opacity: 0.1;
-  }
-
-  2% {
-    opacity: 1;
-  }
-
-  8% {
-    opacity: 0.1;
-  }
-
-  9% {
-    opacity: 1;
-  }
-
-  12% {
-    opacity: 0.1;
-  }
-  20% {
-    opacity: 1;
-  }
-  25% {
-    opacity: 0.3;
-  }
-  30% {
-    opacity: 1;
-  }
-
-  70% {
-    opacity: 0.7;
-  }
-  72% {
-    opacity: 0.2;
-  }
-
-  77% {
-    opacity: 0.9;
-  }
-  100% {
-    opacity: 0.9;
-  }
-}
-
-@keyframes border-flicker {
-  0% {
-    opacity: 0.1;
-  }
-  2% {
-    opacity: 1;
-  }
-  4% {
-    opacity: 0.1;
-  }
-
-  8% {
-    opacity: 1;
-  }
-  70% {
-    opacity: 0.7;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-
-@media only screen and (max-width: 600px) {
-  .glowing-btn{
-    font-size: 6vw;
-  }
-}
-
 
 </style>
+<style src="@/assets/glowingBtn.scss"></style>
